e891348275fa83e24688770921cfca7c630c635c
AlexisDbc <duboc.alexis@orange.fr>
2015-05-27 13:50:14 +0200

    Essai ajout suppression fichier tempo "Copie Spool"

--- EssaiJobImp/Imprimante.cs
+++ EssaiJobImp/Imprimante.cs
@@ -143,7 +143,7 @@ namespace EssaiJobImp
                                 else
                                 {
                                     break;
-
+                                    System.IO.File.Delete(destFile);
                                 }
                                 supOk = false;
                             }
