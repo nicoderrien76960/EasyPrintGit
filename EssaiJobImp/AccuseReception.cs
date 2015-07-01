f185c44cf4d0bef5c82ac3e11f258be46788ae05
AlexisDbc <duboc.alexis@orange.fr>
2015-07-01 10:18:43 +0200

    Correction espace quand notion duplicata non présente

--- EssaiJobImp/AccuseReception.cs
+++ EssaiJobImp/AccuseReception.cs
@@ -91,7 +91,7 @@ namespace EssaiJobImp
                             {
                                 donneeEntete.Add(s, noeud.InnerText);
                             }
-                            else { donneeEntete.Add("Duplicata", ""); }//                            //
+                            else { donneeEntete.Add("Duplicata", "         "); }//                            //
                         }
                         else
                         {
--- EssaiJobImp/BonPréparation.cs
+++ EssaiJobImp/BonPréparation.cs
@@ -92,7 +92,7 @@ namespace EssaiJobImp
                             {
                                 donneeEntete.Add(s, noeud.InnerText);
                             }
-                            else { donneeEntete.Add("Duplicata", ""); }//                            //
+                            else { donneeEntete.Add("Duplicata", "         "); }//                            //
                         }
                         else
                         {
